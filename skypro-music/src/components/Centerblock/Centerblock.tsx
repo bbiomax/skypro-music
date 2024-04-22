af7e98a902c00e32baf0ae79aa77eb81e6962e9b
bbiomax <bbbiomax@gmail.com>
2024-04-22 16:13:40 +0300

    getTracks api

--- skypro-music/src/components/Centerblock/Centerblock.tsx
+++ skypro-music/src/components/Centerblock/Centerblock.tsx
@@ -1,9 +1,13 @@
+import { getTracks } from "@/api/tracks";
 import Filters from "../Filters/Filters";
 import Track from "../Track/Track";
 import styles from "./Centerblock.module.css";
 import classNames from "classnames";
+import { trackType } from "@/types";
 
-export default function Centerblock() {
+export default async function Centerblock() {
+  const tracksData: trackType[] = await getTracks();
+
   return (
     <div className={classNames(styles.mainCenterblock, styles.centerblock)}>
       <div className={classNames(styles.centerblockSearch, styles.search)}>
@@ -19,11 +23,22 @@ export default function Centerblock() {
       </div>
       <h2 className={styles.centerblockH2}>Треки</h2>
       <Filters />
-      <div className={classNames(styles.centerblockContent, styles.playlistContent)}>
+      <div
+        className={classNames(
+          styles.centerblockContent,
+          styles.playlistContent
+        )}
+      >
         <div className={classNames(styles.contentTitle, styles.playlistTitle)}>
-          <div className={classNames(styles.playlistTitleCol, styles.col1)}>Трек</div>
-          <div className={classNames(styles.playlistTitleCol, styles.col2)}>Исполнитель</div>
-          <div className={classNames(styles.playlistTitleCol, styles.col3)}>Альбом</div>
+          <div className={classNames(styles.playlistTitleCol, styles.col1)}>
+            Трек
+          </div>
+          <div className={classNames(styles.playlistTitleCol, styles.col2)}>
+            Исполнитель
+          </div>
+          <div className={classNames(styles.playlistTitleCol, styles.col3)}>
+            Альбом
+          </div>
           <div className={classNames(styles.playlistTitleCol, styles.col4)}>
             <svg className={styles.playlistTitleSvg}>
               <use xlinkHref="img/icon/sprite.svg#icon-watch" />
@@ -31,7 +46,14 @@ export default function Centerblock() {
           </div>
         </div>
         <div className={classNames(styles.contentPlaylist, styles.playlist)}>
-          <Track />
+          {tracksData.map((trackData) => (
+            <Track
+              key={trackData.id}
+              name={trackData.name}
+              author={trackData.author}
+              album={trackData.album}
+            />
+          ))}
         </div>
       </div>
     </div>
